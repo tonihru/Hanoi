769065cf0fc984f2f0ea874b04456dd1e9983b2e
antonia <cc241062@fhstp.ac.at>
2026-03-10 16:22:09 +0100

    set up project and recursive run

---
+++ Hanoi.cs
@@ -0,0 +1,28 @@
+﻿namespace Hanoi;
+
+class Program
+{
+
+
+    static void TowerOfHanoiRecursive(int n, char fromRod, char toRod, char auxRod)
+    {
+        if (n == 0) {
+            return;
+        }
+        TowerOfHanoiRecursive(n - 1, fromRod, auxRod, toRod);
+        Console.WriteLine("Disk " + n + " moved from "
+                          + fromRod + " to " + toRod);
+        TowerOfHanoiRecursive(n - 1, auxRod, toRod, fromRod);
+    }
+    public static void Main(String[] args)
+    {
+        int n = 3;
+
+        TowerOfHanoiRecursive(n, 'L', 'M', 'R');
+    }
+
+    static void TowerOfHanoiIterative()
+    {
+        
+    }
+}
